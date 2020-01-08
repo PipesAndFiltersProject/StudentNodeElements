054de8fec7f2d6ba923ce2c9908146f39ab44266
Antti Juustila <antti.juustila@gmail.com>
2020-01-08 21:06:50 +0200

    Fixed build problems with static libraries

--- OHARStudentLayerConfig.cmake
+++ OHARStudentLayerConfig.cmake
@@ -1,6 +1,6 @@
 include(CMakeFindDependencyMacro)
 
-find_dependency(Boost 1.67.0)
+find_dependency(Boost 1.70.0)
 find_dependency(OHARBaseLayer)
 
 include("${CMAKE_CURRENT_LIST_DIR}/OHARStudentLayerTargets.cmake")
--- do-cmake-thing.sh
+++
@@ -1,4 +0,0 @@
-cd build
-cmake -DCMAKE_PREFIX_PATH=/Users/anttijuustila/workspace/json/build ..
-cd ..
-
