2c81ed112d274dc7b09bf52dd63403f0ce24e1e0
Antti Juustila <antti.juustila@gmail.com>
2020-01-20 15:19:29 +0200

    Fixed build issues with new structure.

--- StudentNodeElementsConfig.cmake
+++ StudentNodeElementsConfig.cmake
@@ -3,4 +3,4 @@ include(CMakeFindDependencyMacro)
 find_dependency(Boost 1.70.0)
 find_dependency(ProcessorNode)
 
-include("${CMAKE_CURRENT_LIST_DIR}/OHARStudentLayerTargets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/StudentNodeElementsTargets.cmake")
